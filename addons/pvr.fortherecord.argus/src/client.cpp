4c60eada2ca7e9db9b758239c582de709ae9b8a6
Marcel Groothuis <margro.xbmc@gmail.com>
2012-09-03 21:59:58 +0200

    Update ForTheRecord and VuPlus pvr addons with GetMininumPVRAPIVersion() function

--- addons/pvr.fortherecord.argus/src/client.cpp
+++ addons/pvr.fortherecord.argus/src/client.cpp
@@ -306,6 +306,12 @@ const char* GetPVRAPIVersion(void)
   return strApiVersion;
 }
 
+const char* GetMininumPVRAPIVersion(void)
+{
+  static const char *strMinApiVersion = XBMC_PVR_MIN_API_VERSION;
+  return strMinApiVersion;
+}
+
 //-- GetAddonCapabilities -----------------------------------------------------
 // Tell XBMC our requirements
 //-----------------------------------------------------------------------------
