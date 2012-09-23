77cac9091a9349f65e9341597897f280f7f6e664
Marcel Groothuis <margro.xbmc@gmail.com>
2012-09-23 18:27:16 +0200

    windows: remove older pvr addons copies from the XBMC tree before copying the new one Make the copy script less verbose

--- scripts/copy_pvr_addon.bat
+++ scripts/copy_pvr_addon.bat
@@ -9,24 +9,34 @@ REM 1st argument should be the source directory of the addon (where the *.pvr fi
 REM 2nd argument should be the wanted pvr addon name (destination folder name) e.g. "pvr.demo"
 REM This script should be executed in the project folder of the addon
 REM e.g. addons\pvr.demo\project\VS2010Express
-echo Current directory: %CD%
+
+REM echo Current directory: %CD%
 SET CUR_DIR=%CD%
 
 SET XBMC_ADDONS_DIR=..\..\..\..\..\addons
 
+REM We have found the XBMC addons folder if it contains the xbmc.pvr library
 IF NOT EXIST "%XBMC_ADDONS_DIR%\library.xbmc.pvr"  EXIT
 
 CD "%XBMC_ADDONS_DIR%"
-ECHO Found the XBMC addons folder at: %CD%
+REM ECHO Found the XBMC addons folder at: %CD%
+
+SET ADDONDSTDIR=%CD%
 SET ADDONSRCDIR=%1
 REM remove the double quotes:
 SET ADDONSRCDIR=%ADDONSRCDIR:"=%
-REM remove the trailing \
+REM remove the trailing \ otherwise xcopy will fail
 IF %ADDONSRCDIR:~-1%==\ SET ADDONSRCDIR=%ADDONSRCDIR:~0,-1%
 
-ECHO Copying the %2 addon from %ADDONSRCDIR% to the XBMC addons folder
+IF EXIST %2 (
+  ECHO Found an existing version of the pvr addon %2 in the XBMC addons folder %ADDONDSTDIR%. Deleting this version...
+  RMDIR /S /Q %2
+)
+
+ECHO Copying the %2 addon from %ADDONSRCDIR% to the XBMC addons folder %ADDONDSTDIR%
+REM exclude some intermediate VS2010 files
 ECHO .ilk>exclude.txt
 ECHO .exp>>exclude.txt
 ECHO .lib>>exclude.txt
-XCOPY "%ADDONSRCDIR%" "%CD%\%2\" /Y /R /I /E /EXCLUDE:exclude.txt
+XCOPY "%ADDONSRCDIR%" "%ADDONDSTDIR%\%2\" /Y /R /I /E /Q /EXCLUDE:exclude.txt
 DEL exclude.txt
