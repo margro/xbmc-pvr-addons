e19c8473322796bbac966ac7735ae9dbf805d7d8
Marcel Groothuis <margro.xbmc@gmail.com>
2014-10-15 23:26:21 +0200

    [VS2013] Fix pvr addon copy with the solution is not called "XBMC for Windows" I'm making a copy before adding the pvr addon to prevent accidentally committing the changed solution file.

--- scripts/copy_pvr_addon.bat
+++ scripts/copy_pvr_addon.bat
@@ -8,35 +8,28 @@ REM Example Post-Build command line: "$(ProjectDir)\..\..\..\..\scripts\copy_pvr
 REM 1st argument should be the source directory of the addon (where the *.pvr file is located)
 REM 2nd argument should be the wanted pvr addon name (destination folder name) e.g. "pvr.demo"
 REM This script should be executed in the project folder of the addon
-REM e.g. addons\pvr.demo\project\VS2010Express
+REM e.g. addons\pvr.demo\project\VS201xExpress
 
 REM echo Current directory: %CD%
 SET CUR_DIR=%CD%
 
 SET XBMC_ADDONS_DIR=..\..\..\..\..\addons
 
-REM We have found the XBMC addons folder if it contains the xbmc.pvr library
-IF NOT EXIST "%XBMC_ADDONS_DIR%\library.xbmc.pvr"  EXIT
-
+REM We have found the XBMC addons folder when it contains the xbmc.pvr library
+IF NOT EXIST "%XBMC_ADDONS_DIR%\library.xbmc.pvr" (
+  ECHO Not building inside the XBMC source tree. Skipped PVR addon copy to the XBMC addons folder.
+  EXIT
+)
 CD "%XBMC_ADDONS_DIR%"
-REM ECHO Found the XBMC addons folder at: %CD%
 
 SET ADDONDSTDIR=%CD%
 SET ADDONSRCDIR=%1
 REM remove the double quotes:
 SET ADDONSRCDIR=%ADDONSRCDIR:"=%
+
 REM remove the trailing \ otherwise xcopy will fail
 IF %ADDONSRCDIR:~-1%==\ SET ADDONSRCDIR=%ADDONSRCDIR:~0,-1%
 
-IF EXIST %2 (
-  ECHO Found an existing version of the pvr addon %2 in the XBMC addons folder %ADDONDSTDIR%. Deleting this version...
-  RMDIR /S /Q %2
-)
-
 ECHO Copying the %2 addon from %ADDONSRCDIR% to the XBMC addons folder %ADDONDSTDIR%
-REM exclude some intermediate VS2010 files
-ECHO .ilk>exclude.txt
-ECHO .exp>>exclude.txt
-ECHO .lib>>exclude.txt
-XCOPY "%ADDONSRCDIR%" "%ADDONDSTDIR%\%2\" /Y /R /I /E /Q /EXCLUDE:exclude.txt
-DEL exclude.txt
+robocopy "%ADDONSRCDIR%" "%ADDONDSTDIR%\%2" *.* /MIR /XF *.ilk *.exp *.lib *.xml.in /NP
+EXIT 0
